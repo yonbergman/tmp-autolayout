510a5955314f5d141df7f3ad0bfa7771da08a96e
yon <Lighthawky@gmail.com>
2015-07-23 10:19:53 +0300

    added images

--- imagetest/ViewController.swift
+++ imagetest/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var imageView: UIImageView!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,10 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBOutlet var tap: UITapGestureRecognizer!
 
+  @IBAction func didTap(sender: AnyObject) {
+    imageView.image = UIImage(named: "tippy-splash")
+  }
 }
 
